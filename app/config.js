f7cd2032ac6ff3d56a1ee7c4f28ed38f77d8d9da
Hack Reactor Students <accounts@hackreactor.com>
2015-01-15 12:22:41 -0800

    something actually workss !

--- app/config.js
+++ app/config.js
@@ -1,45 +1,46 @@
-var Bookshelf = require('bookshelf');
-var path = require('path');
+var mongoose = require('mongoose');
 
-var db = Bookshelf.initialize({
-  client: 'sqlite3',
-  connection: {
-    host: '127.0.0.1',
-    user: 'your_database_user',
-    password: 'password',
-    database: 'shortlydb',
-    charset: 'utf8',
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  }
+var connection = mongoose.connect('mongodb://localhost:27017/local');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('yay, we connected');
 });
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('base_url', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+var Schema = mongoose.Schema;
+
+var urlSchema = new Schema({
+  url:  String,
+  base_url: String,
+  code:   String,
+  title: String,
+  visits: Number
 });
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+var userSchema = new Schema({
+  username:  String,
+  password: String
 });
 
-module.exports = db;
+
+var User = mongoose.model('User', userSchema);
+var Url = mongoose.model('Url', userSchema);
+
+module.exports.User = User;
+module.exports.Url = Url;
+
+// var fred = new User({username: 'fred', password: 'zirdung'});
+
+// fred.save(function(err, fred) {
+//   if (err) { console.log(err) }
+//   else {
+//     console.log('fred saved!');
+//   }
+// });
+
+// User.find({username: 'fred'}, function(error, response) {
+//   console.log(response);
+// })
+
+// db.close();
